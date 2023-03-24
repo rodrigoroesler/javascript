60cacc7041bd7d79dba9aee1b1332596ffd747de
rodrigoroesler <127706303+rodrigoroesler@users.noreply.github.com>
2023-03-24 18:11:35 -0300

    aula 11 adicionada

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var velocidade = 110
+console.log(`A velocidade do seu carro é ${velocidade}km/h`)
+
+if (velocidade > '80') { // Condicao simples
+    console.log('Voce ultrapassou a velocidade permitida. MULTADO')
+} 
+    console.log('Dirija sempre usando cinto de segunranca')
