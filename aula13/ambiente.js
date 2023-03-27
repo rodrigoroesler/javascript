4df1f47bb62c9aa302fce40a07504664f95fbc24
rodrigoroesler <127706303+rodrigoroesler@users.noreply.github.com>
2023-03-27 12:06:28 -0300

    Update ambiente.js

---
+++ aula13/ambiente.js
@@ -0,0 +1,28 @@
+var contador = 1
+do {
+    console.log(`Passo ${contador}`)
+    contador ++
+} while (contador <= 5)
+
+
+var contador = 1
+while (contador <= 5) {
+    console.log(`Passo ${contador}`)
+    contador++
+}
+for (var contador = 1;contador <= 5;contador++)  {
+    console.log(`Passo ${contador}`)
+}
+
+
+
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?') 
+*/
